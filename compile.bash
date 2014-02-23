1fb144bacd59d710f3f2b2c1bce2251a773df9b4
Ben Lubar <ben.lubar@gmail.com>
2014-02-23 17:58:11 -0600

    add darwin compile

--- compile.bash
+++ compile.bash
@@ -2,6 +2,14 @@
 
 set -e
 
+GOOS=darwin   GOARCH=amd64  go build -o untitled-game-mac  "$@"
 GOOS=linux    GOARCH=arm    go build -o untitled-game-arm  "$@"
 GOOS=linux    GOARCH=amd64  go build -o untitled-game      "$@"
 GOOS=windows  GOARCH=amd64  go build -o untitled-game.exe  "$@"
+
+cd prototype/species
+GOOS=darwin   GOARCH=amd64  go build -o species-mac  "$@"
+GOOS=linux    GOARCH=arm    go build -o species-arm  "$@"
+GOOS=linux    GOARCH=amd64  go build -o species      "$@"
+GOOS=windows  GOARCH=amd64  go build -o species.exe  "$@"
+cd ../..
